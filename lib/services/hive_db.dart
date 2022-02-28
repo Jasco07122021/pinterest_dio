863f4e28733dc637381a3255ee1bbde2ab723897
User <jasco.flutter.007>
2022-02-28 08:42:16 +0500

    Initial commit

---
+++ lib/services/hive_db.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+import 'package:hive/hive.dart';
+import 'package:pinterest_2022/models/collection_model.dart';
+
+class HiveDB {
+  static String nameHive = "pinterest";
+  static var box = Hive.box(nameHive);
+
+  static put(List<Collections> list) {
+    List<String> response =
+        List<String>.from(list.map((x) => jsonEncode(x.toJson())));
+    box.put("collections", response);
+  }
+
+  static putSaved(List<Collections> list) {
+    List<String> response =
+        List<String>.from(list.map((x) => jsonEncode(x.toJson())));
+    box.put("save", response);
+  }
+
+  static putUser(Map<dynamic,dynamic> map) {
+    box.put("user", map);
+  }
+
+  static List<Collections> get() {
+    List<String> response = box.get("collections")??[];
+    List<Collections> list = List<Collections>.from(
+        response.map((x) => Collections.fromJson(jsonDecode(x))));
+    return list;
+  }
+
+  static List<Collections> getSaved() {
+    List<String> response = box.get("save")??[];
+    List<Collections> list = List<Collections>.from(
+        response.map((x) => Collections.fromJson(jsonDecode(x))));
+    return list;
+  }
+
+  static Map<dynamic,dynamic> getUser() {
+    Map<dynamic,dynamic> map = box.get("user") ?? {};
+    return map;
+  }
+
+}
